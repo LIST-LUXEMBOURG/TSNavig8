8a6bf370db31191a8241e12bc250e40cbccb3ce1
6G Lab <6glab@6Gs-Mac-Studio.private.list.lu>
2024-03-19 13:19:31 +0100

    Update PlotComponent

--- src/components/PlotLidarComponent.vue
+++ src/components/PlotLidarComponent.vue
@@ -5,9 +5,6 @@
       <div class="d-flex">
         <button class="btn byn-danger m-2" @click.prevent="stop">STOP</button>
         <button class="btn byn-primary m-2" @click.prevent="start">START</button>
-        <button class="btn byn-primary m-2" @click.prevent="increaseCamera">Z CAMERA +</button>
-        <button class="btn byn-primary m-2" @click.prevent="decreaseCamera">Z CAMERA -</button>
-
       </div>
 
     </div>
@@ -17,17 +14,15 @@
 <script setup>
 import * as THREE from 'three';
 import { onMounted, ref, inject, computed, onDeactivated } from "vue";
-
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 const $wsServices = inject('$wsservices')
 let pointsData = computed(() => { return $wsServices.getStore().getLidarData })
-console.log("Lidar: ",pointsData)
 let cameraZ=ref(500)
-
 let scene = null
 let camera = null
 let renderer = null
 let material = null
-
+let controls = null
 
 onMounted(() => {
   $wsServices.connect()
@@ -46,12 +41,31 @@ function sceneInitialisation() {
   // Create camera
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1300);
   //console.log("camera: " + JSON.stringify(camera))
-  camera.position.z = cameraZ.value;
-
+  // camera.position.z = cameraZ.value;
+  camera.position.set(0,0, cameraZ.value)
+  camera.lookAt(scene.position)
   // Create renderer
   const canvas = document.getElementById('lidar-container');
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
   renderer.setSize(1080, 800)
+  
+  
+
+
+  // Create controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.mouseButtons.LEFT = THREE.MOUSE.ROTATE;
+  controls.mouseButtons.RIGHT = THREE.MOUSE.PAN;  
+  controls.maxDistance = 1500;
+  controls.minDistance = 0;
+  // controls.rotateSpeed = 0.5;
+  // controls.panSpeed = 0.5;
+  // controls.screenSpacePanning = false;
+  window.addEventListener('keydown', function(e) {
+    if(e.code === 'KeyR')
+      controls.reset();
+    
+  })
 
   const sprite = new THREE.TextureLoader().load('disc.png');
   material = new THREE.PointsMaterial({
@@ -59,8 +73,7 @@ function sceneInitialisation() {
     map: sprite,
     vertexColors: true // Enable vertex colors
   });
-  // console.log('Scene initialised')
-  // animate()
+
  
 }
 function displayPoints() {
@@ -76,26 +89,21 @@ function displayPoints() {
       positions[i * 3 + 2] = $wsServices.getStore().getLidarData[i][2];
 
       let dst = $wsServices.getStore().getLidarData[i][4]; // distance in cm
-      // let refl = pointsData.value[i][3]; // reflectivity (intensity)
-
       let clr = dst/255;
         colors[i * 3] = 1-clr;
         colors[i * 3 + 1] = clr;
         colors[i * 3 + 2] = clr;
     }
-    // else{
-    //   console.log('else')
-    // }
   }
   geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
   geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
   const pointsThree = new THREE.Points(geometry, material);
   scene.add(pointsThree);
+  // controls.target = new THREE.Vector3(0, 0, 0)
+  // controls.update();
   animate();
-  // geometry.dispose()
-  // positions.dispose()
-  // colors.dispose()
-  // pointsThree.dispose()
+  geometry.dispose(); // to save memory
+  // controls.dispose()
 }
 function animate() {
   scene.remove.apply(scene, scene.children)
@@ -109,29 +117,31 @@ function animate() {
       positions[i * 3 + 2] = $wsServices.getStore().getLidarData[i][2];
 
       let dst = $wsServices.getStore().getLidarData[i][4]; // distance in cm
-      // let refl = pointsData.value[i][3]; // reflectivity (intensity)
-
-      // console.log('distance: ', pointsData.value[i][4])
       let clr = dst/255;
         colors[i * 3] = 1-clr;
         colors[i * 3 + 1] = clr;
         colors[i * 3 + 2] = clr;
     }
-    // else{
-    //   console.log('Here')
-    // }
   }
   const geometry = new THREE.BufferGeometry();
   geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
   geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
   geometry.needsUpdate = true
   material.needsUpdate = true
+  // Create axes
+  // const axesHelper = new THREE.AxesHelper(10000);
+  // scene.add(axesHelper)
   const pointsThree = new THREE.Points(geometry, material);
   scene.add(pointsThree);
   camera.position.z = cameraZ.value
-
+  // camera.position.set(0,0, cameraZ.value)
+  // camera.lookAt(scene.position)
   requestAnimationFrame(animate);
+  // controls.target = new THREE.Vector3(0, 0, 0)
+  controls.update();
   renderer.render(scene, camera);
+  geometry.dispose();  // to save memory
+  // axesHelper.dispose()
 }
 
 function stop(){
@@ -141,15 +151,6 @@ function stop(){
 function start(){
   $wsServices.connect();
   displayPoints();
-}
-
-function increaseCamera(){
-  cameraZ.value = cameraZ.value+40
-}
-
-function decreaseCamera(){
-  cameraZ.value = cameraZ.value-40
-  if (cameraZ.value <=0) cameraZ.value = 0
 }
 
 </script>
